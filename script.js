f14f5c83a2aa9f36efafb8b3b35429123cd1b19c
Tiffany Hill <hill.tiffany365@gmail.com>
2023-08-09 21:26:43 -0400

    The start of this assignment. Added some pseudo code

--- script.js
+++ script.js
@@ -10,7 +10,51 @@ window.addEventListener("load", function() {
        console.log(listedPlanets);
    }).then(function () {
        console.log(listedPlanets);
+       
+       
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
+    //My added code TH
+    const testForm = document.getElementById("testForm");
+	testForm.addEventListener("submit", //add a function here. what happens after it submits);
    
+    // work for input file
+    //.document object (.queryselector)
+    })
+   
+
+    //add validation in here???    
 });
+
+//DOM code here TH
+
+// do I add event listeners for the buttons
+
+
+//pilotstatus.innerHTML = `Pilot {pilot name} is not ready
+//co pilot.innerHTML = `tempate literal.....etc 
+
+//event listner for the updates???
+
+
+//3 if statements
+if (fuel level is too < 10000 lits) {
+    change faultyItems to visible with an updated fuel status stating that there is not enough fuel for the journey
+    
+    fuelSattus.innerHTML = now equals....
+
+    also update launchStatus"Shuttle not ready for launch" and the color should change to red.
+
+}
+
+if (cargo mass is > 10000 kilograms) {
+    change list to visible with....
+    launchStatus changes to...and color changes to...
+}
+
+if (shuttle is ready to launch) {
+    change launchStatus colorto...
+    and display this 'string'..
+}
+
+
+//creat variables/reference div faultyItems, h2 launchStatus
--- scriptHelper.js
+++ scriptHelper.js
@@ -1,40 +1,51 @@
 // Write your helper functions here!
 require('isomorphic-fetch');
 
-function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
-   // Here is the HTML formatting for our mission target div.
-   /*
-                <h2>Mission Destination</h2>
-                <ol>
-                    <li>Name: </li>
-                    <li>Diameter: </li>
-                    <li>Star: ${star}</li>
-                    <li>Distance from Earth: </li>
-                    <li>Number of Moons: </li>
-                </ol>
-                <img src="">
-   */
-}
+// function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
+//    // Here is the HTML formatting for our mission target div.
+//    /*
+//                 <h2>Mission Destination</h2>
+//                 <ol>
+//                     <li>Name: </li>
+//                     <li>Diameter: </li>
+//                     <li>Star: ${star}</li>
+//                     <li>Distance from Earth: </li>
+//                     <li>Number of Moons: </li>
+//                 </ol>
+//                 <img src="">
+//    */
+// }
 
-function validateInput(testInput) {
-   
+function validateInput(testInput) { //is testInput the event?
+   //my code here
+   if ((variable here).value.trim() === "") {
+    window.alert("Oops! You didn't enter anything.");
+    event.preventDefault();
+}
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
+    //validation here
+   //validation for fuel level (is it a number)
+   //pilot name string
+   //if fuellevel and cargolevel is 1000...
 }
 
-async function myFetch() {
-    let planetsReturned;
+// //json
+// async function myFetch() {
+//     let planetsReturned;
 
-    planetsReturned = await fetch().then( function(response) {
-        });
+//     planetsReturned = await fetch().then( function(response) {
+//         });
 
-    return planetsReturned;
-}
+//     return planetsReturned;
+// }
 
-function pickPlanet(planets) {
-}
+// //json
+// function pickPlanet(planets) {
+
+//     //create random   math.random
+// }
 
 module.exports.addDestinationInfo = addDestinationInfo;
 module.exports.validateInput = validateInput;
