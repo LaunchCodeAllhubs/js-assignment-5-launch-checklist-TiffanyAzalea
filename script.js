d81fa05d695aea931e1da9f82b1d645807853861
Tiffany Hill <hill.tiffany365@gmail.com>
2023-08-10 21:10:55 -0400

    Made more changes to files

--- script.js
+++ script.js
@@ -2,6 +2,16 @@
 
 window.addEventListener("load", function() {
 
+const pilot = document.querySelector('input[name=pilotName]').value;
+const coPilot = document.querySelector('input[name=copilotName]').value;
+const fuelLevel = document.querySelector('input[name=fuelLevel]').value;
+const cargoMass = document.querySelector('input[name-cargoMass]').value;
+
+
+//call formsubmission
+//inside of an event listener
+//define variuab le or form
+//
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
    let listedPlanetsResponse;
@@ -10,12 +20,14 @@ window.addEventListener("load", function() {
        console.log(listedPlanets);
    }).then(function () {
        console.log(listedPlanets);
+
+       
        
        
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
     //My added code TH
-    const testForm = document.getElementById("testForm");
-	testForm.addEventListener("submit", //add a function here. what happens after it submits);
+  //  const testForm = document.getElementById("testForm");
+	//testForm.addEventListener("submit", //add a function here. what happens after it submits);
    
     // work for input file
     //.document object (.queryselector)
@@ -30,31 +42,31 @@ window.addEventListener("load", function() {
 // do I add event listeners for the buttons
 
 
-//pilotstatus.innerHTML = `Pilot {pilot name} is not ready
+
 //co pilot.innerHTML = `tempate literal.....etc 
 
 //event listner for the updates???
 
 
-//3 if statements
-if (fuel level is too < 10000 lits) {
-    change faultyItems to visible with an updated fuel status stating that there is not enough fuel for the journey
+//TH 3 if statements
+// if (fuel level is too < 10000 lits) {
+//     change faultyItems to visible with an updated fuel status stating that there is not enough fuel for the journey
     
-    fuelSattus.innerHTML = now equals....
+//     fuelSattus.innerHTML = now equals....
 
-    also update launchStatus"Shuttle not ready for launch" and the color should change to red.
+//     also update launchStatus"Shuttle not ready for launch" and the color should change to red.
 
-}
+// }
 
-if (cargo mass is > 10000 kilograms) {
-    change list to visible with....
-    launchStatus changes to...and color changes to...
-}
+// if (cargo mass is > 10000 kilograms) {
+//     change list to visible with....
+//     launchStatus changes to...and color changes to...
+// }
 
-if (shuttle is ready to launch) {
-    change launchStatus colorto...
-    and display this 'string'..
-}
+// if (shuttle is ready to launch) {
+//     change launchStatus colorto...
+//     and display this 'string'..
+// }
 
 
 //creat variables/reference div faultyItems, h2 launchStatus
--- scriptHelper.js
+++ scriptHelper.js
@@ -1,51 +1,103 @@
 // Write your helper functions here!
 require('isomorphic-fetch');
 
-// function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
-//    // Here is the HTML formatting for our mission target div.
-//    /*
-//                 <h2>Mission Destination</h2>
-//                 <ol>
-//                     <li>Name: </li>
-//                     <li>Diameter: </li>
-//                     <li>Star: ${star}</li>
-//                     <li>Distance from Earth: </li>
-//                     <li>Number of Moons: </li>
-//                 </ol>
-//                 <img src="">
-//    */
-// }
+//Div 1
 
-function validateInput(testInput) { //is testInput the event?
+
+//Div 2
+const faultyItems = document.getElementById("faultyItems");
+const launchStatus = document.getElementById("launchStatus");
+const pilotStatus = document.getElementById("pilotStatus");
+const copilotStatus = document.getElementById("copilotStatus");
+
+function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
+   // Here is the HTML formatting for our mission target div.
+   /*
+   const missionTarget = document.getElementById("missionTarget");
+
+                <h2>Mission Destination</h2>
+                <ol>
+                    <li>Name: </li>
+                    <li>Diameter: </li>
+                    <li>Star: ${star}</li>
+                    <li>Distance from Earth: </li>
+                    <li>Number of Moons: </li>
+                </ol>
+                <img src="">
+   */
+}
+
+function validateInput(testInput) { 
    //my code here
-   if ((variable here).value.trim() === "") {
-    window.alert("Oops! You didn't enter anything.");
-    event.preventDefault();
-}
-}
+   //should I use dot notation here? 
+   //create variables?
+
+    if (testInput === isNaN(value)) {
+        return "Not a Number";
+    }
+
+    if (testInput === "") {
+        return "Empty";
+    }
+
+    if (testInput === typeof Number) {
+        return "Is a Number";
+    }
+
+//validateInput() should take in a string as a parameter and return "Empty", "Not a Number", or "Is a Number" as appropriate.
+
+
+//    if (pilot.value.trim() === "" || coPilot.value.trim() === "" || fuelLevel.value.trim() === "" || cargoMass.value.trim === "") {
+//     window.alert("Please fill out each section.");
+//     event.preventDefault();
+//     //console.log(validateInput());
+// }
+//    if (pilot.value.trim() !== typeof String || coPilot.value.trim() !== typeof String) {
+//     window.alert("Please enter letters only for Pilot and Co-Pilot.");
+//     event.preventDefault();
+//    }
+
+//    if(fuelLevel.value.trim() === isNaN(value) || cargoMass.value.trim() === isNaN(value)) {
+//     window.alert("Please enter only numbers for fuel level or cargo mass.")
+//    }
+// }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
+    // let fuel level = 0;
+    // let cargo mass = 0
+
+    //call validateinput function
+
+
     //validation here
-   //validation for fuel level (is it a number)
    //pilot name string
    //if fuellevel and cargolevel is 1000...
+
+   //does this go in formSubmission function? (update shuttle requirements)
+       pilotStatus.innerHTML = `Pilot ${pilot name} is not ready`
+       copilotStatus.innerHTML = `Co-Pilot ${copilot} is not ready`
+
+       if (fuelLevel < 10000) {
+        faultyItems.style.visibility = 'visible' // is this a funtion or varibable 
+        
+       }
+
+
+// //json
+async function myFetch() {
+    let planetsReturned;
+
+    planetsReturned = await fetch().then( function(response) {
+        });
+
+    return planetsReturned;
 }
 
 // //json
-// async function myFetch() {
-//     let planetsReturned;
+function pickPlanet(planets) {
 
-//     planetsReturned = await fetch().then( function(response) {
-//         });
-
-//     return planetsReturned;
-// }
-
-// //json
-// function pickPlanet(planets) {
-
-//     //create random   math.random
-// }
+    //create random   math.random
+}
 
 module.exports.addDestinationInfo = addDestinationInfo;
 module.exports.validateInput = validateInput;
